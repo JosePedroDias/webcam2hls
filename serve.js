82db151c1643122017b623ef5b1c316819d5e2d1
José Pedro Dias <jose.pedro.dias@gmail.com>
2014-11-21 03:35:58 +0000

    out with express

--- serve.js
+++ serve.js
@@ -1,42 +1,41 @@
 var fs = require('fs');
+var http = require('http');
 
-var express = require('express');
-
-var pageContent = fs.readFileSync('page.html').toString();
+var files = {
+	'/':                        ['text/html',              fs.readFileSync('page.html').toString()],
+	'/MediaStreamRecorder.js':  ['application/javascript', fs.readFileSync('node_modules/msr/MediaStreamRecorder.js').toString()]
+};
 
 var PORT = 8000;
 
-var app = express();
+var respond = function(res, pair) {
+	res.writeHeader(200, {'Content-Type': pair[0]});
+	res.write(pair[1]);  
+	res.end();
+};
 
-app.use('/node_modules', express.static(__dirname + '/node_modules'));
-
-app.get('/', function(req, res) {
-    res.end(pageContent);
-});
-
-app.post('/chunk/:name/:count', function(req, res) {
-
-	try {
-		var n = req.params.name;
-		var c = req.params.count;
-		var f = n + '_' + c + '.webm';
-
+http.createServer(function(req, res) {
+	var u = req.url;
+    console.log(u);
+    var f = files[u];
+    if (f) {
+    	respond(res, f);	
+    }
+    else if (u.indexOf('/chunk/') === 0) {
+    	var p = u.split('/');
+    	var n = p[1];
+    	var c = p[2];
+    	var f = n + '_' + c + '.webm';
 		console.log('got chunk %s %s', n, c);
-
 		var stream = fs.createWriteStream(f);
-
 		stream.on('end', function() {
-			res.end([ 'GOT', n, c, '->', f].join(' '));
+			respond(res, ['text/plain', ['GOT', n, c, '->', f].join(' ') ] );
 		});
-
-		//console.log(req); res.end(':P');
-
-		req.socket.pipe(stream);
-	} catch (ex) {
-		res.end('ERROR: ' + ex.message);
-	}
-});
-
-var server = app.listen(PORT, function() {
-    console.log('MediaStreamRecorder app listening to port %s...', PORT);
-});
+		req.setEncoding('binary');
+		req.pipe(stream);
+    }
+    else {
+    	respond(res, ['text/plain', 'WAT? ' + u]);
+    }
+}).listen(PORT);  
+console.log('MediaStreamRecorder app listening to port %s...', PORT);
